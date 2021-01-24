45f368f691bb8a7a4fd4adfbabf33a40382f9411
iamseki <chrisyuri_19@hotmail.com>
2021-01-24 12:49:44 -0300

    test: ensure returns bad request if currentDate or investmentDate is invalid

--- src/presentation/controllers/calculate-cdb.spec.ts
+++ src/presentation/controllers/calculate-cdb.spec.ts
@@ -3,25 +3,38 @@ import {
   CalculateUnitCDB,
   ComputedCDB,
 } from '../../domain/calculate-unit-cdb';
+import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
+import { DateValidator } from '../protocols/date-validator';
 import { CalculateCDBController } from './calculate-cdb';
 
-const makeCalculateUnitCDBStub = (): CalculateUnitCDB => {
+const makeCalculateUnitCDB = (): CalculateUnitCDB => {
   class CalculateUnitCDBStub implements CalculateUnitCDB {
-    async compute(params: CalculateCDBParams): Promise<ComputedCDB> {
+    async compute(params: CalculateCDBParams): Promise<ComputedCDB[]> {
       throw new Error('Method not implemented.');
     }
   }
   return new CalculateUnitCDBStub();
 };
 
+const makeDateValidator = (): DateValidator => {
+  class DateValidatorStub implements DateValidator {
+    isValid(date: string): boolean {
+      return true;
+    }
+  }
+  return new DateValidatorStub();
+};
+
 const makeSut = () => {
-  const calculator = makeCalculateUnitCDBStub();
-  const sut = new CalculateCDBController(calculator);
+  const calculator = makeCalculateUnitCDB();
+  const dateValidator = makeDateValidator();
+  const sut = new CalculateCDBController(calculator, dateValidator);
   return {
-    calculator,
+    calculatorStub: calculator,
     sut,
+    dateValidatorStub: dateValidator,
   };
 };
 
@@ -64,5 +77,19 @@ describe('CalculateCDB', () => {
     expect(httpResponse).toEqual(
       badRequest(new MissingParamError('currentDate'))
     );
+  });
+
+  test('Should return 400 if invalid date is provided', async () => {
+    const { sut, dateValidatorStub } = makeSut();
+    jest.spyOn(dateValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const httpRequest = {
+      body: {
+        currentDate: new Date('2016-12-26'),
+        cdbRate: 103.5,
+        investmentDate: new Date('2016-12-26'),
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('date')));
   });
 });
