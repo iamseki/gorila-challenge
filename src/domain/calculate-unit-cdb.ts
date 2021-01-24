398961846b06ba463d5ec36448ccf8e7216937d9
iamseki <chrisyuri_19@hotmail.com>
2021-01-24 12:00:52 -0300

    feat: calculate unit cdb interfaces

---
+++ src/domain/calculate-unit-cdb.ts
@@ -0,0 +1,14 @@
+export interface CalculateCDBParams {
+  investmentDate: Date;
+  cdbRate: number;
+  currentDate: Date;
+}
+
+export interface ComputedCDB {
+  date: Date;
+  unitPrice: number;
+}
+
+export interface CalculateUnitCDB {
+  compute: (params: CalculateCDBParams) => Promise<ComputedCDB>;
+}
