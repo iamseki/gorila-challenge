827a1f779d515b55044a27f7291e020c2bd16814
iamseki <chrisyuri_19@hotmail.com>
2021-01-25 18:24:19 -0300

    feat: ensure return correct cdi range between given dates

---
+++ src/infra/db/mongodb/cdi-repository.ts
@@ -0,0 +1,18 @@
+import { CDI, CDIRepository } from '@/app/protocols/cdi-repository';
+import { MongoHelper } from './mongo-helper';
+
+export class CDIMongoRepository implements CDIRepository {
+  async findBetweenDate(start: Date, end: Date): Promise<CDI[]> {
+    const cdiCollection = await MongoHelper.getCollection<CDI>('cdi');
+    const cdis = await cdiCollection
+      .find({
+        date: {
+          $gte: start,
+          $lt: end,
+        },
+      })
+      .toArray();
+
+    return cdis;
+  }
+}
