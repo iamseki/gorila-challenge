e1906f90a5873b168a57788f01854a9d26d50626
iamseki <chrisyuri_19@hotmail.com>
2021-01-25 15:29:05 -0300

    test: ensure DbCalculateUnitCDB return computed results properly if success

--- src/app/usecases/db-calculate-unit-cdb.spec.ts
+++ src/app/usecases/db-calculate-unit-cdb.spec.ts
@@ -2,14 +2,22 @@ import {
   CalculateCDBParams,
   ComputedCDB,
 } from '../../domain/calculate-unit-cdb';
-import { CDIRepository } from '../protocols/cdi-repository';
+import { CDI, CDIRepository } from '../protocols/cdi-repository';
 import { ComputedCDBCacheRepository } from '../protocols/computed-cache-repository';
 import { DBCalculateUnitCDB } from './db-calculate-unit-cdb';
 
+const makeMockCDI = (): CDI[] => [
+  { date: new Date('2016-11-14'), value: 13.88 },
+  { date: new Date('2016-11-16'), value: 13.88 },
+  { date: new Date('2016-11-17'), value: 13.88 },
+  { date: new Date('2016-11-18'), value: 13.88 },
+  { date: new Date('2016-11-21'), value: 13.88 },
+];
+
 const makeCDIRepository = (): CDIRepository => {
   class CDIRepositoryStub implements CDIRepository {
-    async findByDate(date: string): Promise<number> {
-      return new Promise((resolve) => resolve(13.88));
+    async findBetweenDate(start: Date, end: Date): Promise<CDI[]> {
+      return new Promise((resolve) => resolve(makeMockCDI()));
     }
   }
   return new CDIRepositoryStub();
@@ -37,9 +45,9 @@ const makeCacheRepository = (): ComputedCDBCacheRepository => {
 };
 
 const makeInputCDBParams = (): CalculateCDBParams => ({
-  currentDate: new Date('2016-12-23'),
   cdbRate: 103.5,
-  investmentDate: new Date('2016-12-21'),
+  investmentDate: new Date('2016-11-14'),
+  currentDate: new Date('2016-11-21'),
 });
 
 const makeSut = () => {
@@ -62,5 +70,28 @@ describe('DBCalculateUnitCDB usecase', () => {
     const cacheSpy = jest.spyOn(computedCacheRepositoryStub, 'get');
     await sut.compute(inputCDBParams);
     expect(cacheSpy).toHaveBeenCalledWith(inputCDBParams);
+  });
+
+  it('Should search for a CDI in CDIRepository with correct values', async () => {
+    const { cdiRepositoryStub, sut } = makeSut();
+    const inputCDBParams = makeInputCDBParams();
+    const repositorySpy = jest.spyOn(cdiRepositoryStub, 'findBetweenDate');
+    await sut.compute(inputCDBParams);
+    const { currentDate, investmentDate } = inputCDBParams;
+    expect(repositorySpy).toHaveBeenCalledWith(investmentDate, currentDate);
+  });
+
+  it('Should return correct computed cdb values if success', async () => {
+    const { sut } = makeSut();
+    const inputCDBParams = makeInputCDBParams();
+    const expectedComputedCDB: ComputedCDB[] = [
+      { date: new Date('2016-11-14'), unitPrice: 1000.53397 },
+      { date: new Date('2016-11-16'), unitPrice: 1001.06822 },
+      { date: new Date('2016-11-17'), unitPrice: 1001.60276 },
+      { date: new Date('2016-11-18'), unitPrice: 1002.13758 },
+    ];
+
+    const computedCDB = await sut.compute(inputCDBParams);
+    expect(computedCDB).toEqual(expectedComputedCDB);
   });
 });
